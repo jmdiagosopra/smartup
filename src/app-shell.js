fde333657c15fca008f765f79023a8337d052af1
Arturo Zarzalejo <jose-manuel.diago@soprasteria.com>
2023-02-02 17:21:36 +0100

    prueba

--- src/app-shell.js
+++ src/app-shell.js
@@ -5,6 +5,7 @@ import { installOfflineWatcher } from 'pwa-helpers/network.js';
 import { installRouter } from 'pwa-helpers/router.js';
 import { updateMetadata } from 'pwa-helpers/metadata.js';
 import { arrowDown, heartIcon } from './components/my-icons.js';
+import { Router } from '@vaadin/router';
 
 import './components/lit-i18n';
 import { get as i18n } from './components/lit-i18n';
@@ -371,9 +372,9 @@ class AppShell extends connect(store)(LitElement) {
         <div class="navigation-buttons">
           <nav>
             <ul class="toolbar-list">
-              <li><a ?selected="${this._page === 'training'}" href="./training"><lit-i18n>TRAINING</lit-i18n></a></li>
-              <li><a ?selected="${this._page === 'community'}" href="./community"><lit-i18n>COMMUNITY</lit-i18n></a></li>
-              <li><a ?selected="${this._page === 'career'}" href="./career"><lit-i18n>CAREER</lit-i18n></a></li>
+              <li><a ?selected="${this._page === 'training'}" href="./training" @click="${() => Router.go('./training')}"><lit-i18n>TRAINING</lit-i18n></a></li>
+              <li><a ?selected="${this._page === 'community'}" href="/smartup/community"><lit-i18n>COMMUNITY</lit-i18n></a></li>
+              <li><a ?selected="${this._page === 'career'}" href="/smartup/career"><lit-i18n>CAREER</lit-i18n></a></li>
             </ul>
           </nav>
           <a class="primary-button big" id="want-know-btn" href="mailto:bedigital@soprasteria.com?subject=SmartUp+Xperience"><lit-i18n raw>WANT_TO_BE_SMARTER_BTN</lit-i18n></a>
@@ -383,9 +384,9 @@ class AppShell extends connect(store)(LitElement) {
         </div>
         <div ?opened="${this._drawerOpened}" class="drawer-list">
           <a ?selected="${this._page === 'home'}" href="/home"><lit-i18n>HOME</lit-i18n></a>
-          <a ?selected="${this._page === 'training'}" href="./training"><lit-i18n>SMARTUP_TRAINING</lit-i18n></a>
-          <a ?selected="${this._page === 'community'}" href="./community"><lit-i18n>SMARTUP_COMMUNITY</lit-i18n></a>
-          <a ?selected="${this._page === 'career'}" href="./career"><lit-i18n>SMARTUP_CAREER</lit-i18n></a>
+          <a ?selected="${this._page === 'training'}" href="/smartup/training"><lit-i18n>SMARTUP_TRAINING</lit-i18n></a>
+          <a ?selected="${this._page === 'community'}" href="/smartup/community"><lit-i18n>SMARTUP_COMMUNITY</lit-i18n></a>
+          <a ?selected="${this._page === 'career'}" href="/smartup/career"><lit-i18n>SMARTUP_CAREER</lit-i18n></a>
         </div>
       </header>
       
